c5ef2595873bb6659f72721a34e3b97b239eb51b
Zeussurvival <emiljokui@gmail.com>
2025-09-19 15:19:47 +0200

    NoMore de rotate, collision a faire et position en carré depuis le 0

--- Test_py.py
+++ Test_py.py
@@ -21,19 +21,18 @@ print(img_dir)
 
 class Imagee():
     def __init__(self,name):
-        self.img = pygame.image.load(os.path.join(img_dir, name)).convert_alpha()
+        self.real_img = pygame.image.load(os.path.join(img_dir, name)).convert_alpha()
+        self.real_img = pygame.transform.scale(self.real_img,(64,64))
+        self.img = self.real_img
         self.rect = self.img.get_rect()
-        self.rotaton = 0
-    def rotate(self,rotation):
-        self.rotation = rotation
-        self.rect = self.img.get_rect()
+        # self.rotation = 0  et  https://stackoverflow.com/questions/4183208/how-do-i-rotate-an-image-around-its-center-using-pygame
     def draw_self(self,pos):
         real_pos = pos[0] - self.rect[2] / 2, pos[1] - self.rect[3] / 2
+        print(pos)
         screen.blit(self.img,real_pos)
     def print_rect(self):
         print(self.rect)
 
-    
 
 Img_Humain = Imagee("Player.png")
 Wall1 = W.Walls(S_WIDTH - 180, 20,160,60)
@@ -44,7 +43,7 @@ with open("Items.json","r") as f:
     Items = json.loads(f.read())
 print(Items)
 
-Human1 = H.Humanoid(500,500,80,80,300,"Humain","Bob",pygame.image.load(os.path.join(img_dir,'Player.png')))
+Human1 = H.Humanoid(S_WIDTH/2,S_HEIGHT/2,80,80,300,"Humain","Bob",Img_Humain)
 Zombie1 = H.Humanoid(500,500,80,80,300,"Zombie","Zomb1",None)
 Zombie_liste = [Zombie1] 
 
@@ -67,14 +66,13 @@ while running:
     mouse_pos = pygame.mouse.get_pos()
     mouse_click = pygame.mouse.get_pressed()
     
-
-    Human1.moove(keys,dt)
-    Img_Humain.draw_self(Human1.pos)
     for obj in Wall_liste:
         pygame.draw.rect(screen,"white",W.Walls.rect(Wall1))
-    for zombie in Zombie_liste:
-        pygame.draw.rect(screen,"red",zombie.Rect(),0,10)
+    # for zombie in Zombie_liste:
+    #     pygame.draw.rect(screen,"red",zombie.Rect(),0,10)
     
+    Human1.moove(keys,dt)
+    Img_Humain.draw_self(Human1.pos)
     if mouse_click == (True,False,False) and not clicked:
         Deagle.shoot()
         clicked = True
@@ -82,7 +80,7 @@ while running:
         clicked = False
     if keys[pygame.K_r]:
         Deagle.reload()
-    Img_Humain.print_rect()
+    
     Deagle.update()
     pygame.display.flip()
     dt = clock.tick(60) / 1000
